14d8f2353eeaff360b186bf7a35c3efd14214f39
Arti Chetu <artij@chetu.com>
2021-10-27 21:51:56 +0530

    added bootstrap on show page

--- app/helpers/follows_helper.rb
+++ app/helpers/follows_helper.rb
@@ -3,7 +3,7 @@ module FollowsHelper
   def follow_toggle(article)
     text = already_followed?(article) ? "Unfollow" : "Follow"
 
-    button_to text, article_follows_path(article), method: :post, class: 'btn btn-primary btn-sm'
+    button_to text, article_follows_path(article), method: :post
   end
 
   def already_followed?(article)
